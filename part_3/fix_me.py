d7979cd5e41f85c5d69fe302461d9a56f8f48bb1
clavia96 <olo0002@auburn.edu>
2020-02-20 15:25:10 -0600

    fix_me.py

--- part_3/fix_me.py
+++ part_3/fix_me.py
@@ -1,90 +1,91 @@
-
 #!/usr/bin/env python3
 
 # Reading data from the user's input
-a = input("Enter a : ")
 
-print("You entered", a, "which is a", type(a))
+value = input("Enter integer: ")
+a = int(value)
 
-b = input("Enter b : ")
+print("You entered",a, "which is a", type(a))
 
-print("You entered", b, "which is a", type(b))
+valuee = input("Enter integer: ")
+b = int(valuee)
+
+print("You entered",b, "which is a", type(b))
 
 
 #######################################################################
 # HINT: why would we be checking what type a and b are again?
 #       Let's assume we want a and b to be integers at this point
 #######################################################################
-
-print("What is", a, "now?", type(a))
-print("What is", b, "now?", type(a))
-
-
-total = a + b
-print("a + b =", total, ", which is a", type(total))
-
-difference = a - b
-print("a - b =", difference, ", which is a", type(difference))
-
-product = a * b
-print("a * b =", product, ", which is a", type(product))
-
-quotient = a / b
-print("a / b =", quotient, ", which is a", type(quotient))
-
-floor_quotient = a // b
-print("a // b =",
-        floor_quotient,
-        ", which is a",
-        type(floor_quotient),
-        )
-
-remainder = a % b
-print("a % b =", remainder, ", which is a", type(remainder))
-
-power = a ** b
-print("a ** b =", power, ", which is a", type(power))
-
-a += 1
-print("Incrementing \"a\" by one results in", a, "which is a", type(a))
-
-b -= 1
-print("Decrementing \"b\" by one results in", b, "which is a", type(b))
-
-a += 1.0
-print("Incrementing \"a\" by 1.0 results in", a, "which is a", type(a))
-
-
-print("Now \"a\" equals", a, "and is a", type(a))
-print("Now \"b\" equals", b, "and is a", type(b))
+print("What is",a, "now?", type(a))
+print("What is",b, "now?", type(a))
 
 
 total = a + b
- print("a + b =", total, ", which is a", type(total))
+print("a + b =",total, ", which is a", type(total))
 
-difference = a - b
-print("a - b =", difference, ", which is a", type(difference))
+#difference = a - b
+#print("a - b =", difference, ", which is a", type(difference))
 
-    product = a * b
-print("a * b =", product, ", which is a", type(product))
+#product = a * b
+#print("a * b =", product, ", which is a", type(product))
 
-quotient = a / b
-print("a / b =", quotient, ", which is a", type(quotient))
+#quotient = a / b
+#print("a / b =", quotient, ", which is a", type(quotient))
 
-floor_quotient = a // b
-print("a // b =", floor_quotient, ", which is a", type(floor_quotient))
+#floor_quotient = a // b
+#print("a // b =",
+        #floor_quotient,
+        #", which is a",
+        #type(floor_quotient),
+        #)
 
-remainder = a % b
-print("a % b =," remainder ", which is a," type(remainder))
+#remainder = a % b
+#print("a % b =", remainder, ", which is a", type(remainder))
 
-power = a ** b
-print("a ** b =",
-        power,
-        ", which is a",
-        type(power),
-        )
+#power = a ** b
+#print("a ** b =", power, ", which is a", type(power))
 
-a += 1
-print("Incrementing \"a\" by one results in", a, "which is a", type(a))
+#a += 1
+#print("Incrementing \"a\" by one results in", a, "which is a", type(a))
 
-print("Woohoo! Nicely done!")
+#b -= 1
+#print("Decrementing \"b\" by one results in", b, "which is a", type(b))
+
+#a += 1.0
+#print("Incrementing \"a\" by 1.0 results in", a, "which is a", type(a))
+
+
+#print("Now \"a\" equals", a, "and is a", type(a))
+#print("Now \"b\" equals", b, "and is a", type(b))
+
+
+#total = a + b
+ #print("a + b =", total, ", which is a", type(total))
+
+#difference = a - b
+#print("a - b =", difference, ", which is a", type(difference))
+
+    #product = a * b
+#print("a * b =", product, ", which is a", type(product))
+
+#quotient = a / b
+#print("a / b =", quotient, ", which is a", type(quotient))
+
+#floor_quotient = a // b
+#print("a // b =", floor_quotient, ", which is a", type(floor_quotient))
+
+#remainder = a % b
+#print("a % b =," remainder ", which is a," type(remainder))
+
+#power = a ** b
+#print("a ** b =",
+        #power,
+        #", which is a",
+        #type(power),
+        #)
+
+#a += 1
+#print("Incrementing \"a\" by one results in", a, "which is a", type(a))
+
+#print("Woohoo! Nicely done!")
